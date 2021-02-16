5bae9a6ffa847c97875a179e8f568ef61a320106
ariesteanila <aries.teanila@yahoo.com>
2021-02-16 09:51:08 +0800

    add initial method

---
+++ APICallTasker/APICallTasker/TaskerForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace APICallTasker
+{
+    partial class TaskerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bgWorker = new System.ComponentModel.BackgroundWorker();
+            this.txtVIN = new System.Windows.Forms.TextBox();
+            this.lblVIN = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgList = new System.Windows.Forms.DataGridView();
+            this.c_Tasks = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bgWorker
+            // 
+            this.bgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorker_DoWorkAsync);
+            this.bgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RunWorkerCompleted);
+            // 
+            // txtVIN
+            // 
+            this.txtVIN.Location = new System.Drawing.Point(12, 12);
+            this.txtVIN.Name = "txtVIN";
+            this.txtVIN.Size = new System.Drawing.Size(220, 20);
+            this.txtVIN.TabIndex = 0;
+            this.txtVIN.Text = "2C4RC1DGXMR505860";
+            // 
+            // lblVIN
+            // 
+            this.lblVIN.AutoSize = true;
+            this.lblVIN.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVIN.Location = new System.Drawing.Point(12, 35);
+            this.lblVIN.Name = "lblVIN";
+            this.lblVIN.Size = new System.Drawing.Size(74, 16);
+            this.lblVIN.TabIndex = 1;
+            this.lblVIN.Text = "Input VIN:";
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Location = new System.Drawing.Point(7, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(542, 47);
+            this.label1.TabIndex = 2;
+            // 
+            // dgList
+            // 
+            this.dgList.AllowUserToAddRows = false;
+            this.dgList.AllowUserToDeleteRows = false;
+            this.dgList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.c_Tasks});
+            this.dgList.Location = new System.Drawing.Point(7, 58);
+            this.dgList.Name = "dgList";
+            this.dgList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgList.Size = new System.Drawing.Size(542, 238);
+            this.dgList.TabIndex = 3;
+            // 
+            // c_Tasks
+            // 
+            this.c_Tasks.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.c_Tasks.HeaderText = "Tasks";
+            this.c_Tasks.Name = "c_Tasks";
+            // 
+            // TaskerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(561, 308);
+            this.Controls.Add(this.dgList);
+            this.Controls.Add(this.lblVIN);
+            this.Controls.Add(this.txtVIN);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "TaskerForm";
+            this.Text = "Tasks Form";
+            this.Load += new System.EventHandler(this.TaskerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker bgWorker;
+        private System.Windows.Forms.TextBox txtVIN;
+        private System.Windows.Forms.Label lblVIN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgList;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c_Tasks;
+    }
+}
+
---
+++ APICallTasker/APICallTasker/TaskerForm.cs
@@ -0,0 +1,81 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APICallTasker
+{
+    public partial class TaskerForm : Form
+    {
+        //TODO: set baseUri in Config
+        private static string baseUri = "https://ebcx7wvx2d.execute-api.us-east-1.amazonaws.com";
+        private static HttpClient client = new HttpClient();
+
+        public TaskerForm()
+        {
+            InitializeComponent();
+
+            client.BaseAddress = new Uri(baseUri);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+        }
+
+        private void TaskerForm_Load(object sender, EventArgs e)
+        {
+            if (!bgWorker.IsBusy)
+                bgWorker.RunWorkerAsync(3000);
+        }
+
+        private void BgWorker_DoWorkAsync(object sender, DoWorkEventArgs e)
+        {
+            var result = GetTask(txtVIN.Text);
+            e.Result = (string)result;
+        }
+
+        private void bgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            // First, handle the case where an exception was thrown.
+            if (e.Error != null)
+            {
+                MessageBox.Show(e.Error.Message);
+            }
+            else if (e.Cancelled)
+            {
+                // CancelAsync was called.
+                //DO NOTHING
+            }
+            else
+            {
+                // Finally, handle the case where the operation
+                // succeeded.
+                if(e.Result != null)
+                {
+                    dgList.Rows.Add(e.Result as string);
+                    dgList.Refresh();
+                    dgList.PerformLayout();
+
+                    bgWorker.RunWorkerAsync(3000);
+                }
+            }
+        }
+
+        #region Methods
+        private string GetTask(string vin)
+        {
+            var result = client.GetStringAsync($"/default/mhits/tasks?vin={vin}").GetAwaiter().GetResult();
+            return result.ToString();
+        }
+        #endregion
+
+    }
+}
